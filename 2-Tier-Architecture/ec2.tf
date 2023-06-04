88afb009d6b3f19f5ea5dfe3e35fd232239f0f47
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-06-04 23:57:24 +0530

    updated ec2.tf

--- 2-Tier-Architecture/ec2.tf
+++ 2-Tier-Architecture/ec2.tf
@@ -1,11 +1,10 @@
-#creating EC2 instance
 resource "aws_instance" "My-web-instance1" {
   ami                         = "ami-02f3f602d23f1659d" #Amazon linux 2 AMI
   key_name                    = "mykeypair"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public-subnet1.id
+  subnet_id                   = aws_subnet.Public_subnet-1.id
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.Custom-Public-SG-DB.id]
+  vpc_security_group_ids      = [aws_security_group.custom_security_SG_DB.id]
   user_data                   = <<-EOF
         #!/bin/bash
         yum update -y
@@ -17,12 +16,12 @@ resource "aws_instance" "My-web-instance1" {
 }
 
 resource "aws_instance" "My-web-instance2" {
-  ami                         = "ami-02f3f602d23f1659d" #Amazon linux 2 AMI 
+  ami                         = "ami-02f3f602d23f1659d" #Amazon linux 2 AMI
   key_name                    = "mykeypair"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public-subnet2.id
+  subnet_id                   = aws_subnet.Public_subnet-2.id
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.Custom-Public-SG-DB.id]
+  vpc_security_group_ids      = [aws_security_group.custom_security_SG_DB.id]
   user_data                   = <<-EOF
         #!/bin/bash
         yum update -y
